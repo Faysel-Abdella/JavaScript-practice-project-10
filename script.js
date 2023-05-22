d220d6d71602b06543c855faf8aeb964a6b0bac9
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-05-22 19:09:11 +0100

    The play button is activated using JS.

---
+++ script.js
@@ -0,0 +1,51 @@
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+// Song titles
+const songs = ["Track-1", "Track-2", "Track-3"]; // Array of titles should match the song file name
+
+let songIndex = 2;
+
+loadSong(songs[songIndex]);
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Event listeners
+playBtn.addEventListener("click", () => {
+  const isplaying = musicContainer.classList.contains("play");
+
+  if (isplaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
